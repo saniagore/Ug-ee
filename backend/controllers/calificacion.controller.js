68d93ea822dedba4e3b9e6fe0fd11743e2c4b685
Santiago Vanegas T <santiagov.050506@gmail.com>
2025-05-27 13:50:52 -0500

    🔧 Development: 📝 calificacion.controller.js
    Los cambios en `calificacion.controller.js` son puramente de formato. Se corrigió la indentación y el espaciado alrededor de la declaración `async` y el bloque `try...catch` en la función `calificarViaje`, mejorando la legibilidad del código sin alterar su funcionalidad.

--- backend/controllers/calificacion.controller.js
+++ backend/controllers/calificacion.controller.js
@@ -3,15 +3,36 @@ import { DB_CONFIG } from "../config.js";
 const { Pool } = pg;
 const pool = new Pool(DB_CONFIG);
 
-export const calificarViaje = async(viajeId, calificacion, comentario) =>{
-    try{
+export const calificarViaje = async(viajeId, calificacion, comentario) => {
+    try {
+
         const result = await pool.query(`
             INSERT INTO calificacion (puntuacion, comentario, viajeId)
-            VALUES ($1,$2,$3)
-            RETURNING *`, [calificacion,comentario,viajeId])
+            VALUES ($1, $2, $3)
+            RETURNING *`, 
+            [calificacion, comentario, viajeId]
+        );
+
+        await pool.query(`
+            UPDATE conductor c
+            SET puntuacionPromedio = (
+                SELECT AVG(puntuacion) 
+                FROM calificacion cal
+                JOIN viaje v ON cal.viajeId = v.id
+                JOIN vehiculo ve ON v.vehiculoId = ve.id
+                WHERE ve.conductorId = c.cId
+            )
+            WHERE c.cId = (
+                SELECT ve.conductorId
+                FROM viaje v
+                JOIN vehiculo ve ON v.vehiculoId = ve.id
+                WHERE v.id = $1
+            )`, 
+            [viajeId]
+        );
 
         return result.rows;
-    }catch(error){
+    } catch(error) {
         throw error;
     }
 };
